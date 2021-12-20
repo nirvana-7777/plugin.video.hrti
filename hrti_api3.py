05579e18045d81579bab8e5a15af56c87ed4a174
nirvana-7777 <48356537+nirvana-7777@users.noreply.github.com>
2021-12-20 22:43:17 +0100

    Create hrti_api3.py

---
+++ hrti_api3.py
@@ -0,0 +1,18 @@
+
+  
+import requests
+import time
+import base64
+import json
+
+import xbmc
+
+
+class HRTiAPI:
+    user_agent = "kodi plugin for hrti.hr (python)"
+
+    def __init__(self, username, password):
+        self._auth = None
+        self.logged_in = False
+        self.__username = username
+        self.__password = password
