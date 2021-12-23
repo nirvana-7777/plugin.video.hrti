369f132c17abaeef5fa3477d96596a813660615a
nirvana-7777 <nirv_inc@yahoo.com>
2021-12-23 20:23:40 +0100

    Test

--- lib/hrti_api2.py
+++ lib/hrti_api2.py
@@ -64,8 +64,9 @@ class HRTiAPI:
             self.logged_in = True
             # result = r.get('Result')
             # token = result.get()
-            parsed_json = (json.loads(r.json()))
-            print(json.dumps(parsed_json, indent=4, sort_keys=True))
+            data = dumps(r).encode('utf-8')
+            # print(json.dumps(parsed_json, indent=4, sort_keys=True))
             # xbmc.log("hrti grant access: " + str(r.json()), level=xbmc.LOGDEBUG)
+            xbmc.log("hrti grant access: " + data, level=xbmc.LOGDEBUG)
             # self._auth["expires"] = time.time() + self._auth["expires_in"]
         return r.status_code
