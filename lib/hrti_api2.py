19d6bc4fdad45b2d06680598cf6e09a4539104d3
nirvana-7777 <nirv_inc@yahoo.com>
2021-12-23 20:54:29 +0100

    Test

--- lib/hrti_api2.py
+++ lib/hrti_api2.py
@@ -54,7 +54,7 @@ class HRTiAPI:
                    'sec-fetch-site': 'same-origin',
                    'sec-fetch-mode': 'cors',
                    'sec - fetch - dest': 'empty',
-                   'Cookie': 'G_ENABLED_IDPS=google; __gfp_64b=dvp3hBZb2fPYy3qzSZtVq.Ry9tKP.Qk5fq.vGYYpDin.27|1638888286; g_state={"i_p":1640256389790,"i_l":3,"i_t":1640340704876}; ',
+                   # 'Cookie': 'G_ENABLED_IDPS=google; __gfp_64b=dvp3hBZb2fPYy3qzSZtVq.Ry9tKP.Qk5fq.vGYYpDin.27|1638888286; g_state={"i_p":1640256389790,"i_l":3,"i_t":1640340704876}; ',
                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
         self._auth = None
         r = requests.post(url, data=payload, headers=headers)
