d34a4193d6e3f50bfc582e8d1f8a2aa77790b883
nirvana-7777 <nirv_inc@yahoo.com>
2021-12-24 17:55:21 +0100

    Test

--- lib/hrti_api2.py
+++ lib/hrti_api2.py
@@ -20,18 +20,20 @@ class HRTiAPI:
         self.__username = username
         self.__password = password
         self.__ip = self.get_ip(self)
+        self.session = requests.session()
         # self.__device_id = "a8dc5ca6-8932-4932-88b6-6aee5d843624"
         xbmc.log("hrti init with IP: " + str(self.__ip), level=xbmc.LOGDEBUG)
         xbmc.log("hrti init with User: " + username, level=xbmc.LOGDEBUG)
         xbmc.log("hrti init with PW: " + password, level=xbmc.LOGDEBUG)
-        xbmc.log("hrti init with Cookie: " + str(self.cookie), level=xbmc.LOGDEBUG)
+        xbmc.log("hrti init with Cookie: " + str(self.session.cookies), level=xbmc.LOGDEBUG)
         self.grant_access()
 
     @staticmethod
     def get_ip(self):
         url = "https://hrti.hrt.hr/api/api/ott/getIPAddress"
-        r = requests.get(url)
-        self.cookie = r.cookies
+        r = self.session.get(url)
+        # r = requests.get(url)
+        # self.cookie = r.cookies
         return r.json()
 
     def grant_access(self):
@@ -62,7 +64,8 @@ class HRTiAPI:
                    # 'Cookie': 'G_ENABLED_IDPS=google; __gfp_64b=dvp3hBZb2fPYy3qzSZtVq.Ry9tKP.Qk5fq.vGYYpDin.27|1638888286; g_state={"i_p":1640256389790,"i_l":3,"i_t":1640340704876}; ',
                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
         self._auth = None
-        r = requests.post(url, json=payload, headers=headers, cookies=self.cookie)
+        # r = requests.post(url, json=payload, headers=headers, cookies=self.cookie)
+        r = self.session.post(url, json=payload, headers=headers)
         xbmc.log("hrti status code: " + str(r.status_code), level=xbmc.LOGDEBUG)
         if r.status_code == 200:
             # self._auth = r.json()
