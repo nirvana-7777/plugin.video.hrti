77f54f9561bbff0f8bbeb1cdb56eb618ce6de2f2
nirvana-7777 <nirv_inc@yahoo.com>
2021-12-27 21:51:07 +0100

    Test

--- lib/hrti_api2.py
+++ lib/hrti_api2.py
@@ -32,6 +32,7 @@ class HRTiAPI:
         self.grant_access()
         self.register_device()
         self.get_content_rating()
+        self.get_profiles()
 
     @staticmethod
     def get_ip(self):
@@ -211,6 +212,39 @@ class HRTiAPI:
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
             'x-postman-captr': '4178146'
+        }
+
+        response = self.session.post(url, headers=headers, data=payload)
+
+        print(response.headers.get('content-type'))
+        print(response.text)
+        return response.status_code
+
+    def get_profiles(self):
+
+        url = self.hsapiBaseUrl + "/client.svc/json/ProfilesGet"
+
+        payload = json.dumps({})
+
+        headers = {
+            'host': 'hsapi.aviion.tv',
+            'connection': 'keep-alive',
+            'content-length': '2',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+            'accept': 'application/json, text/plain, */*',
+            'content-type': 'application/json',
+            'authorization': 'Client '+self.__token,
+            'sec-ch-ua-mobile': '?0',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+            'sec-ch-ua-platform': '"Linux"',
+            'origin': 'https://hrti.hrt.hr',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'referer': 'https://hrti.hrt.hr/',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+            'x-postman-captr': '6006285'
         }
 
         response = self.session.post(url, headers=headers, data=payload)
