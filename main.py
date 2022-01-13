b35b82ab3d2ef7214c0623f3e6ca3c53233b0f6b
nirvana-7777 <nirv_inc@yahoo.com>
2022-01-13 17:20:21 +0100

    Test

--- main.py
+++ main.py
@@ -266,7 +266,10 @@ def list_epg(channel):
         programme = programmes[0]
         epglist = plugin.get_dict_value(programme, 'EpgList')
         for item in epglist:
-            list_item = xbmcgui.ListItem(label=plugin.get_dict_value(item, 'Title'))
+            timestart = plugin.get_date_from_epoch(plugin.get_dict_value(item, 'TimeStart'))
+            timeend = plugin.get_date_from_epoch(plugin.get_dict_value(item, 'TimeEnd'))
+            entry = plugin.get_dict_value(item, 'Title') + " | " + str(timestart) + "-" + str(timeend)
+            list_item = xbmcgui.ListItem(label=entry)
             list_item.setArt({'thumb': plugin.get_dict_value(item, 'ImagePath'),
                               'icon': plugin.get_dict_value(item, 'ImagePath'),
                               'fanart': plugin.get_dict_value(item, 'ImagePath')})
