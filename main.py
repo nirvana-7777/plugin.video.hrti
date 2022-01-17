07554b67e9dd6b7b295402b3fd8c0ff882cfd340
nirvana-7777 <nirv_inc@yahoo.com>
2022-01-17 12:23:27 +0100

    Test

--- main.py
+++ main.py
@@ -297,14 +297,12 @@ def list_epg(channel):
         timenow = plugin.get_datetime_now()
         for item in epglist:
             timestart = ""
-            # print(timenow)
-            # print(plugin.get_datetime_from_epoch(plugin.get_dict_value(item, 'TimeStart')))
             timestart = timestart + str(plugin.get_date_from_epoch(plugin.get_dict_value(item, 'TimeStart')))
             timestart = timestart + " | " + str(plugin.get_time_from_epoch(plugin.get_dict_value(item, 'TimeStart')))
             entry = timestart + " | " + plugin.get_dict_value(item, 'Title')
-            list_item = xbmcgui.ListItem(label=entry)
             if timenow > plugin.get_datetime_from_epoch(plugin.get_dict_value(item, 'TimeStart')):
-                list_item = xbmcgui.ListItem('[B][I][COLOR green]%s[/COLOR][/I][/B]' % label)
+                entry = '[COLOR green]' + entry + '[/COLOR]'
+            list_item = xbmcgui.ListItem(label=entry)
             list_item.setArt({'thumb': plugin.get_dict_value(item, 'ImagePath'),
                               'icon': plugin.get_dict_value(programme, 'Icon'),
                               'fanart': plugin.get_dict_value(item, 'ImagePath')})
