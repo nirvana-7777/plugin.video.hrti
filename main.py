2c73398aa4d5567c15427a54da448609eb6b2ee3
nirvana-7777 <nirv_inc@yahoo.com>
2022-01-20 15:21:57 +0100

    Release Version 0.9:             - Fixed: Radio             - Added: EPG             - Added: Restart from EPG (green items)             - Added: EPG Details for Radio / TV             - Added: Plot / Description

--- main.py
+++ main.py
@@ -423,6 +423,7 @@ def play_video(path, epg_ref_id):
     parts = urlparse(path)
     if epg_ref_id is None:
         voddetails = api.get_vod_details(path)
+        print(voddetails)
         filename = plugin.get_dict_value(voddetails, 'FileName')
         content_type = plugin.get_dict_value(voddetails, 'Type')
         video_store_ids = plugin.get_dict_value(voddetails, 'SVODVideostores')
