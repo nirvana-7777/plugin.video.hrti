73a7216fa8ff799164142c33e1ca5b7f4a61104b
nirvana-7777 <nirv_inc@yahoo.com>
2022-07-26 11:32:42 +0200

    Release Version 0.9.8:             - Fix EPG

--- main.py
+++ main.py
@@ -264,7 +264,6 @@ def list_videos(category):
                         channel_epg = plugin.get_dict_value(programme, 'EpgList')
                 label = plugin.get_dict_value(channel, 'Name')
                 if channel_epg is not None:
-                    print(channel_epg)
                     now = get_now_event(channel_epg)
                     if now is not None:
                         label += str(" | ") + plugin.get_dict_value(now, 'Title')
