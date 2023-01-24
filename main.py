9971976c493798ad0c82cba1d99725bc77e94167
nirvana-7777 <nirv_inc@yahoo.com>
2023-01-24 17:20:52 +0100

    Release Version 1.0.0:             - Release for Kodi 20 Nexus

--- main.py
+++ main.py
@@ -626,7 +626,7 @@ def play_video(path, epg_ref_id):
                         content_type = "rlive"
                     else:
                         content_type = "tlive"
-                    print("Now: " + plugin.get_datetime_now())
+                    print("Now: " + plugin.time_now())
                     authorize_and_play(path, content_type, refid, None, refid, epg_ref_id, None, None)
 
 
