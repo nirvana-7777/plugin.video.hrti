591d0dcd68af45e61b900d45692254c511f1aad2
nirvana-7777 <nirv_inc@yahoo.com>
2023-01-26 17:18:06 +0100

    Release for Kodi 20 (Nexus)

--- lib/hrti_api.py
+++ lib/hrti_api.py
@@ -238,6 +238,7 @@ class HRTiAPI:
         })
         referer = self.hrtiDomain + "/videostore"
         result = self.api_post(url, payload, referer)
+        print(result)
         return result
 
     def get_programme(self, channelids, starttime, endtime):
